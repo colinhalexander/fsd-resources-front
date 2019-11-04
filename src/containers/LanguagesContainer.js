99d32c1657af1a030adecef997af2cd31be49a48
colinhalexander <chalexander@icloud.com>
2019-11-04 10:06:26 -0700

    add languages container and display components

---
+++ src/containers/LanguagesContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class LanguagesContainer extends Component {
+
+  render() {
+    return (
+      <div className="languages-container">
+      </div>
+    )
+  }
+}
